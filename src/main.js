7206bef26630980c0a27720e762991af522d1603
Insp1rer <igorkariymk@gmail.com>
2024-04-13 14:37:42 +0300

    щось

--- src/main.js
+++ src/main.js
@@ -10,3 +10,37 @@ new Accordion(accordions, {
   showMultiple: false,
   openOnInit: [0],
 });
+
+import Swiper from 'swiper';
+import { Navigation, Mousewheel, Keyboard } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+let params = {
+  modules: [Navigation, Mousewheel, Keyboard],
+
+  slidesPerView: 2,
+  // maxwidth: 262,
+  loop: true,
+  centeredSlides: false,
+  direction: 'horizontal',
+  mousewheel: true,
+  keyboard: true,
+  freeMode: true,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 6,
+    },
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
+};
+
+const swiper = new Swiper('.swiper', params);
